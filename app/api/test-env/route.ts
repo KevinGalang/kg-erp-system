b7dc4309d4e8cc044f02bcd43b2d74f0bd66b134
VidalKevin <kevin@vidalcoaching.com>
2026-05-31 12:28:35 -0400

    Add environment variable test route

---
+++ app/api/test-env/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    supabaseUrl: !!process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseAnon: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    serviceRole: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+    shopDomain: !!process.env.SHOPIFY_STORE_DOMAIN,
+  });
+}
